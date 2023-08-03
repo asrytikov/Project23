28f772e7f2e0baa3408053f0753732c7b5b416d6
asrytikov <asrytikov@gmail.com>
2023-08-03 20:21:40 +0400

    Project 23

--- src/main/java/com/example/project23/controllers/PurchaseController.java
+++ src/main/java/com/example/project23/controllers/PurchaseController.java
@@ -1,2 +1,31 @@
-package com.example.project23.controllers;public class PurchaseController {
+package com.example.project23.controllers;
+
+import com.example.project23.model.Purchase;
+import com.example.project23.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase){
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases(){
+        return purchaseRepository.findAllPurchase();
+    }
+
+
+
 }
